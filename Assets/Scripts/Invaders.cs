6654610e9a0753bb795374d03e471dff7be0d36f
Hnikar <example@example.com>
2024-07-03 01:09:36 +0200

    Add Invaders movement

--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -1,30 +1,110 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Invaders : MonoBehaviour
 {
-    public Invader[] prefabs;
+    [Header("Invaders")]
+    public Invader[] prefabs = new Invader[5];
+    public AnimationCurve speed = new AnimationCurve();
 
+    private Vector3 direction = Vector3.right;
+    private Vector3 initialPosition;
+
+    [Header("Grid")]
     public int rows = 5;
-
     public int columns = 11;
 
-    private void Awake(){
-        for(int row = 0; row < this.rows; row++){
 
-        float width = 2.0f * (this.columns - 1);
-        float height = 2.0f * (this.rows - 1);
-        Vector2 centering = new Vector2(-width / 2, -height / 2);
-        Vector3 rowPosition = new Vector3(centering.x, centering.y +(row *2.0f), 0.0f);
+    private void Awake()
+    {
+        initialPosition = transform.position;
 
-           for (int col = 0; col < this.columns; col++)
+        CreateInvaderGrid();
+    }
+
+    private void CreateInvaderGrid()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            float width = 2f * (columns - 1);
+            float height = 2f * (rows - 1);
+
+            Vector2 centerOffset = new Vector2(-width * 0.5f, -height * 0.5f);
+            Vector3 rowPosition = new Vector3(centerOffset.x, (2f * i) + centerOffset.y, 0f);
+
+            for (int j = 0; j < columns; j++)
             {
-                Invader invader = Instantiate(this.prefabs[row], this.transform);
+                Invader invader = Instantiate(prefabs[i], transform);
+
                 Vector3 position = rowPosition;
-                position.x += col * 2.0f;
-                invader.transform.position = position;
+                position.x += 2f * j;
+                invader.transform.localPosition = position;
             }
         }
     }
+
+    private void Update()
+    {
+        int totalCount = rows * columns;
+        int amountAlive = GetAliveCount();
+        int amountKilled = totalCount - amountAlive;
+        float percentKilled = (float)amountKilled / (float)totalCount;
+
+        float speed = this.speed.Evaluate(percentKilled);
+        transform.position += speed * Time.deltaTime * direction;
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        foreach (Transform invader in transform)
+        {
+            if (!invader.gameObject.activeInHierarchy) {
+                continue;
+            }
+
+            if (direction == Vector3.right && invader.position.x >= (rightEdge.x - 1f))
+            {
+                AdvanceRow();
+                break;
+            }
+            else if (direction == Vector3.left && invader.position.x <= (leftEdge.x + 1f))
+            {
+                AdvanceRow();
+                break;
+            }
+        }
+    }
+
+    private void AdvanceRow()
+    {
+        direction = new Vector3(-direction.x, 0f, 0f);
+
+        Vector3 position = transform.position;
+        position.y -= 1f;
+        transform.position = position;
+    }
+
+    public void ResetInvaders()
+    {
+        direction = Vector3.right;
+        transform.position = initialPosition;
+
+        foreach (Transform invader in transform) {
+            invader.gameObject.SetActive(true);
+        }
+    }
+
+    public int GetAliveCount()
+    {
+        int count = 0;
+
+        foreach (Transform invader in transform)
+        {
+            if (invader.gameObject.activeSelf) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
 }
