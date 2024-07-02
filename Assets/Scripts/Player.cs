0e3788adf31728d24c9470d9ffe23bb20601d82b
Hnikar <example@example.com>
2024-07-03 01:16:27 +0200

    Add player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    
+    public float speed = 5.0f;
+
+    private void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            position.x -= speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            position.x += speed * Time.deltaTime;
+        }
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
+
+        transform.position = position;
+    }
+}
